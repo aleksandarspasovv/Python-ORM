14c9b1dbeda338cbe21f4dbcc9d38a62e2cc9a2e
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-08-22 03:14:48 +0300

    updates

--- lecture_17/main_app/models.py
+++ lecture_17/main_app/models.py
@@ -1,3 +1,18 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
+from validators import ValidateName, ValidateAge
+
+
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(
+        max_length=100,
+        validators=[ValidateName('Name can only contain letters and spaces')]
+    )
+    age = models.PositiveIntegerField(
+        validators=MinLengthValidator(18, message='Age must be greater than or equal to 18')
+    )
+    email = models.EmailField()
