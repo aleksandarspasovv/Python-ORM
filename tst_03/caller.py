5ace98629932d3d82649ccfd8b28d02ca8bd26e9
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-08-27 04:04:07 +0300

    updates

--- tst_03/caller.py
+++ tst_03/caller.py
@@ -2,9 +2,22 @@ import os
 import django
 from datetime import date
 
+
+
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+from main_app.models import Student
+from django.db import connection
+from pprint import pprint
 # Run and print your queries
+
+students = Student.objects.all()
+
+
+for s in students:
+    print(f'{s.first_name} {s.last_name} has email {s.email}')
+
+pprint(Student.objects.all().query)
--- tst_03/main_app/models.py
+++ tst_03/main_app/models.py
@@ -1,4 +1,5 @@
-from django.db import models
+from django.db import models, connection
+
 
 class Student(models.Model):
     student_id = models.CharField(max_length=10, unique=True, primary_key=True)
@@ -7,5 +8,11 @@ class Student(models.Model):
     birth_date = models.DateField(null=True, blank=True)
     email = models.EmailField(unique=True)
 
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+
+class Seller(models.Model):
+    name = models.CharField(max_length=150)
+
+
+class Order(models.Model):
+    seller = models.ForeignKey(Seller, related_name="orders", on_delete=models.PROTECT)
+    total = models.DecimalField(max_digits=18, decimal_places=9)
